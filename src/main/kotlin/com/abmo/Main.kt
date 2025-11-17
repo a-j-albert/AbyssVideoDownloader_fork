d90ff01be3a438313fa66109b6dc74fbd7a5b23f
a-j-albert <aalbert@cyberdude.com>
2025-11-17 13:39:58 -0800

    Added commented Throw and sleep for debugging

--- src/main/kotlin/com/abmo/Main.kt
+++ src/main/kotlin/com/abmo/Main.kt
@@ -3,8 +3,9 @@ package com.abmo
 import com.abmo.di.koinModule
 import org.koin.core.context.startKoin
 
-
 suspend fun main(args: Array<String>) {
     startKoin { modules(koinModule) }
+    // throw RuntimeException("Condition met, breaking for debug.");
+    // Thread.sleep(30000)
     Application(args).run()
 }
